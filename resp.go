f1d7e22d2b9192ea77025d56baaacb973514bdb0
yongj.zhuang <yongj.zhuang@outlook.com>
2024-04-03 13:11:20 +0800

    Change Value to value type, remove use of ptr

--- resp.go
+++ resp.go
@@ -41,20 +41,22 @@ var (
 	ErrUnexpectedEndOfBulkStr = errors.New("invalid protocol, unexpected end of bulk string")
 	ErrInvalidTypeForCommand  = errors.New("invalid data type for command")
 	ErrEmptyPayload           = errors.New("invalid protocol, empty payload")
+
+	NilVal = Value{}
 )
 
 var (
 	Separator       = []byte{'\r', '\n'}
-	CommandHandlers = map[string]func(args []*Value) *Value{
-		"PING": func(args []*Value) *Value {
-			return &Value{
+	CommandHandlers = map[string]func(args []Value) Value{
+		"PING": func(args []Value) Value {
+			return Value{
 				typ:  SimpleStringsTyp,
 				strv: "PONG",
 			}
 		},
-		"SET": func(args []*Value) *Value {
+		"SET": func(args []Value) Value {
 			if len(args) < 2 {
-				return &Value{typ: SimpleErrorsTyp, err: ErrInvalidArgument}
+				return Value{typ: SimpleErrorsTyp, err: ErrInvalidArgument}
 			}
 
 			// TODO: Parse the args to support EX seconds | PX milliseconds
@@ -76,29 +78,29 @@ var (
 
 			prev, ok := Lookup(args[0].strv)
 			if (ok && nx) || (xx && !ok) { // NX and exists or XX and not exists
-				return &Value{typ: NullsTyp}
+				return Value{typ: NullsTyp}
 			}
 
 			SetVal(args[0].strv, args[1].strv)
 			if ok {
-				return &Value{typ: BulkStringsTyp, strv: cast.ToString(prev)}
+				return Value{typ: BulkStringsTyp, strv: cast.ToString(prev)}
 			}
-			return &Value{typ: SimpleStringsTyp, strv: "OK"}
+			return Value{typ: SimpleStringsTyp, strv: "OK"}
 		},
-		"GET": func(args []*Value) *Value {
+		"GET": func(args []Value) Value {
 			if len(args) < 1 {
-				return &Value{typ: SimpleErrorsTyp, err: ErrInvalidArgument}
+				return Value{typ: SimpleErrorsTyp, err: ErrInvalidArgument}
 			}
 
 			prev, ok := Lookup(args[0].strv)
 			if !ok {
-				return &Value{typ: NullsTyp}
+				return Value{typ: NullsTyp}
 			}
-			return &Value{typ: BulkStringsTyp, strv: cast.ToString(prev)}
+			return Value{typ: BulkStringsTyp, strv: cast.ToString(prev)}
 		},
-		"DEL": func(args []*Value) *Value {
+		"DEL": func(args []Value) Value {
 			if len(args) < 1 {
-				return &Value{typ: IntegersTyp, intv: 0}
+				return Value{typ: IntegersTyp, intv: 0}
 			}
 			cnt := int64(0)
 			for _, ar := range args {
@@ -108,120 +110,120 @@ var (
 					DelKey(ar.strv)
 				}
 			}
-			return &Value{typ: IntegersTyp, intv: cnt}
+			return Value{typ: IntegersTyp, intv: cnt}
 		},
-		"INCR": func(args []*Value) *Value {
+		"INCR": func(args []Value) Value {
 			if len(args) < 1 {
-				return &Value{typ: SimpleErrorsTyp, err: ErrInvalidArgument}
+				return Value{typ: SimpleErrorsTyp, err: ErrInvalidArgument}
 			}
 			prev, ok := Lookup(args[0].strv)
 			if !ok {
 				SetVal(args[0].strv, int64(1))
-				return &Value{typ: IntegersTyp, intv: 1}
+				return Value{typ: IntegersTyp, intv: 1}
 			}
 			pv, ok := prev.(int64)
 			if !ok {
-				return &Value{typ: SimpleErrorsTyp, err: ErrInvalidInt}
+				return Value{typ: SimpleErrorsTyp, err: ErrInvalidInt}
 			}
 			SetVal(args[0].strv, pv+1)
-			return &Value{typ: IntegersTyp, intv: pv + 1}
+			return Value{typ: IntegersTyp, intv: pv + 1}
 		},
-		"DECR": func(args []*Value) *Value {
+		"DECR": func(args []Value) Value {
 			if len(args) < 1 {
-				return &Value{typ: SimpleErrorsTyp, err: ErrInvalidArgument}
+				return Value{typ: SimpleErrorsTyp, err: ErrInvalidArgument}
 			}
 			prev, ok := Lookup(args[0].strv)
 			if !ok {
 				SetVal(args[0].strv, int64(0))
-				return &Value{typ: IntegersTyp, intv: 0}
+				return Value{typ: IntegersTyp, intv: 0}
 			}
 			pv, ok := prev.(int64)
 			if !ok {
-				return &Value{typ: SimpleErrorsTyp, err: ErrInvalidInt}
+				return Value{typ: SimpleErrorsTyp, err: ErrInvalidInt}
 			}
 			SetVal(args[0].strv, pv-1)
-			return &Value{typ: IntegersTyp, intv: pv - 1}
+			return Value{typ: IntegersTyp, intv: pv - 1}
 		},
-		"EXPIRE": func(args []*Value) *Value {
+		"EXPIRE": func(args []Value) Value {
 			if len(args) < 2 { // EXPIRE $KEY $SECONDS
-				return &Value{typ: SimpleErrorsTyp, err: ErrInvalidArgument}
+				return Value{typ: SimpleErrorsTyp, err: ErrInvalidArgument}
 			}
 
 			k := args[0].strv
 			_, ok := Lookup(k)
 			if !ok {
-				return &Value{typ: IntegersTyp, intv: 0}
+				return Value{typ: IntegersTyp, intv: 0}
 			}
 
 			n := args[1].strv
 			ttl := CalcExp(cast.ToInt64(n), time.Second)
 			SetExpire(k, ttl)
-			return &Value{typ: IntegersTyp, intv: 1}
+			return Value{typ: IntegersTyp, intv: 1}
 		},
-		"PEXPIRE": func(args []*Value) *Value {
+		"PEXPIRE": func(args []Value) Value {
 			if len(args) < 2 { // EXPIRE $KEY $MILLISECONDS
-				return &Value{typ: SimpleErrorsTyp, err: ErrInvalidArgument}
+				return Value{typ: SimpleErrorsTyp, err: ErrInvalidArgument}
 			}
 
 			k := args[0].strv
 			_, ok := Lookup(k)
 			if !ok {
-				return &Value{typ: IntegersTyp, intv: 0}
+				return Value{typ: IntegersTyp, intv: 0}
 			}
 
 			n := args[1].strv
 			ttl := CalcExp(cast.ToInt64(n), time.Millisecond)
 			SetExpire(k, ttl)
-			return &Value{typ: IntegersTyp, intv: 1}
+			return Value{typ: IntegersTyp, intv: 1}
 		},
-		"TTL": func(args []*Value) *Value {
+		"TTL": func(args []Value) Value {
 			if len(args) < 1 {
-				return &Value{typ: SimpleErrorsTyp, err: ErrInvalidArgument}
+				return Value{typ: SimpleErrorsTyp, err: ErrInvalidArgument}
 			}
 
 			k := args[0].strv
 			_, ok := Lookup(k)
 			if !ok {
-				return &Value{typ: IntegersTyp, intv: -2}
+				return Value{typ: IntegersTyp, intv: -2}
 			}
 			ttl := LoadTTL(k, false)
-			return &Value{typ: IntegersTyp, intv: ttl}
+			return Value{typ: IntegersTyp, intv: ttl}
 		},
-		"PTTL": func(args []*Value) *Value {
+		"PTTL": func(args []Value) Value {
 			if len(args) < 1 {
-				return &Value{typ: SimpleErrorsTyp, err: ErrInvalidArgument}
+				return Value{typ: SimpleErrorsTyp, err: ErrInvalidArgument}
 			}
 			k := args[0].strv
 			_, ok := Lookup(k)
 			if !ok {
-				return &Value{typ: IntegersTyp, intv: -2}
+				return Value{typ: IntegersTyp, intv: -2}
 			}
 			ttl := LoadTTL(k, true)
-			return &Value{typ: IntegersTyp, intv: ttl}
+			return Value{typ: IntegersTyp, intv: ttl}
 		},
 	}
 )
 
 var (
-	TypWriter = map[byte]func(v *Value, out []byte) []byte{
-		BulkStringsTyp: func(v *Value, out []byte) []byte {
+	TypWriter = map[byte]func(v Value, out []byte) []byte{
+		BulkStringsTyp: func(v Value, out []byte) []byte {
 			out = append(out, cast.ToString(len(v.strv))...)
 			out = append(out, Separator...)
 			out = append(out, v.strv...)
 			return out
 		},
-		SimpleStringsTyp: func(v *Value, out []byte) []byte {
+		SimpleStringsTyp: func(v Value, out []byte) []byte {
 			out = append(out, v.strv...)
 			return out
 		},
-		SimpleErrorsTyp: func(v *Value, out []byte) []byte {
+		SimpleErrorsTyp: func(v Value, out []byte) []byte {
 			out = append(out, v.err.Error()...)
 			return out
 		},
-		NullsTyp: func(v *Value, out []byte) []byte {
+		NullsTyp: func(v Value, out []byte) []byte {
 			return out
 		},
-		IntegersTyp: func(v *Value, out []byte) []byte {
+		IntegersTyp: func(v Value, out []byte) []byte {
 			out = append(out, cast.ToString(v.intv)...)
 			return out
 		},
@@ -245,25 +247,25 @@ func TokenToStr(tokens [][]byte) []string {
 	return st
 }
 
-func parseArray(reader *RespReader) (*Value, error) {
-	b, ok := reader.ReadByte()
+func parseArray(reader *RespReader) (Value, error) {
+	b, ok := reader.MoveOne()
 	if !ok {
-		return nil, ErrExpectArrayEle
+		return NilVal, ErrExpectArrayEle
 	}
 	n := cast.ToInt(string(b))
 
-	elements := make([]*Value, 0, n)
+	elements := make([]Value, 0, n)
 	for i := 0; i < n; i++ {
 		ele, err := parseNext(reader)
 		if err != nil {
-			return nil, fmt.Errorf("failed to parse next array element, %w", err)
+			return NilVal, fmt.Errorf("failed to parse next array element, %w", err)
 		}
 		elements = append(elements, ele)
 	}
-	return &Value{typ: ArraysTyp, arr: elements}, nil
+	return Value{typ: ArraysTyp, arr: elements}, nil
 }
 
-func execute(v *Value, err error) []byte {
+func execute(v Value, err error) []byte {
 	if err != nil {
 		return writeErr(err)
 	}
@@ -284,7 +286,7 @@ func execute(v *Value, err error) []byte {
 	if *debug {
 		fmt.Printf("%v Command: %v,", NowStr(), name)
 		for i, ar := range args {
-			fmt.Printf("%#v,", *ar)
+			fmt.Printf("%#v,", ar)
 			if i < len(args)-1 {
 				fmt.Print(", ")
 			}
@@ -294,7 +296,7 @@ func execute(v *Value, err error) []byte {
 	return writeResult(handler(args))
 }
 
-func writeResult(v *Value) []byte {
+func writeResult(v Value) []byte {
 	out := []byte{}
 	out = append(out, v.typ)
 	out = TypWriter[v.typ](v, out)
@@ -305,7 +307,7 @@ func writeResult(v *Value) []byte {
 func writeErr(err error) []byte {
 	out := []byte{}
 	out = append(out, SimpleErrorsTyp)
-	out = TypWriter[SimpleErrorsTyp](&Value{typ: SimpleErrorsTyp, err: err}, out)
+	out = TypWriter[SimpleErrorsTyp](Value{typ: SimpleErrorsTyp, err: err}, out)
 	out = append(out, Separator...)
 	return out
 }
@@ -319,13 +321,13 @@ func ParseRespProto(reader *RespReader) []byte {
 	case ArraysTyp:
 		return execute(parseNext(reader))
 	default:
-		return execute(nil, ErrExpectArray)
+		return execute(NilVal, ErrExpectArray)
 	}
 }
 
-func parseNext(reader *RespReader) (*Value, error) {
+func parseNext(reader *RespReader) (Value, error) {
 	reader.SkipSeparator()
-	b, _ := reader.ReadByte()
+	b, _ := reader.MoveOne()
 	switch b {
 	case BulkStringsTyp:
 		return parseBulkString(reader)
@@ -335,11 +337,11 @@ func parseNext(reader *RespReader) (*Value, error) {
 		return parseArray(reader)
 	default:
 		fmt.Printf("> parseNext: Invalid data type: '%s'\n", string(b))
-		return nil, fmt.Errorf("invalid protocol, type %s not recognized or not supported", string(b))
+		return NilVal, fmt.Errorf("invalid protocol, type %s not recognized or not supported", string(b))
 	}
 }
 
-func parseSimpleString(reader *RespReader) (*Value, error) {
+func parseSimpleString(reader *RespReader) (Value, error) {
 	buf := GetBuf()
 	defer PutBuf(buf)
 
@@ -351,17 +353,17 @@ func parseSimpleString(reader *RespReader) (*Value, error) {
 		if b == '\r' {
 			b2, ok := reader.PeekNext()
 			if ok && b2 == '\n' {
-				reader.Move(2)
+				reader.Skip(2)
 				break
 			}
 		}
 		buf = append(buf, b)
-		reader.Move(1)
+		reader.Skip(1)
 	}
-	return &Value{typ: SimpleStringsTyp, strv: string(buf)}, nil
+	return Value{typ: SimpleStringsTyp, strv: string(buf)}, nil
 }
 
-func parseBulkString(reader *RespReader) (*Value, error) {
+func parseBulkString(reader *RespReader) (Value, error) {
 	buf := GetBuf()
 	defer PutBuf(buf)
 	for {
@@ -371,13 +373,13 @@ func parseBulkString(reader *RespReader) (*Value, error) {
 		}
 		if b >= '0' && b <= '9' {
 			buf = append(buf, b)
-			reader.Move(1)
+			reader.Skip(1)
 		} else {
 			break
 		}
 	}
 	if len(buf) < 1 {
-		return nil, ErrExpectBulkStr
+		return NilVal, ErrExpectBulkStr
 	}
 
 	n := cast.ToInt(string(buf))
@@ -385,20 +387,20 @@ func parseBulkString(reader *RespReader) (*Value, error) {
 
 	reader.SkipSeparator()
 	for i := 0; i < n; i++ {
-		b, ok := reader.ReadByte()
+		b, ok := reader.MoveOne()
 		if !ok {
-			return nil, ErrUnexpectedEndOfBulkStr
+			return NilVal, ErrUnexpectedEndOfBulkStr
 		}
 		buf = append(buf, b)
 	}
-	return &Value{typ: BulkStringsTyp, strv: string(buf)}, nil
+	return Value{typ: BulkStringsTyp, strv: string(buf)}, nil
 }
 
 type Value struct {
 	typ  byte
 	strv string
 	intv int64
-	arr  []*Value
+	arr  []Value
 	err  error
 }
 
@@ -407,7 +409,7 @@ type RespReader struct {
 	Buf []byte
 }
 
-func (r *RespReader) ReadByte() (byte, bool) {
+func (r *RespReader) MoveOne() (byte, bool) {
 	if r.Pos >= len(r.Buf) {
 		return 0, false
 	}
@@ -416,7 +418,7 @@ func (r *RespReader) ReadByte() (byte, bool) {
 	return b, true
 }
 
-func (r *RespReader) Move(i int) {
+func (r *RespReader) Skip(i int) {
 	r.Pos += i
 }
 
@@ -448,7 +450,7 @@ func (r *RespReader) SkipSeparator() {
 	if b1 == '\r' {
 		b2, ok := r.PeekNext()
 		if ok && b2 == '\n' {
-			r.Move(2)
+			r.Skip(2)
 		}
 	}
 }
